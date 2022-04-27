add714ff43173ccac626c29e462b03d768ec8ce9
"moonman369" <"mightyayan369@gmail.com">
2022-04-28 00:01:06 +0530

    commit-27.4.22-3

--- contract/token.sol
+++ contract/token.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
 import "./tokenvesting.sol";
 
 contract TokenXYZ is  ERC20 {
--- contract/tokenvesting.sol
+++ contract/tokenvesting.sol
@@ -25,7 +25,10 @@ contract TokenVesting is Ownable {
     }
 
     uint256 private schemeId;
+    uint256 private schemeCountLimit;
+    uint256 private schemeCount;
     mapping (uint256 => vestingScheme) schemes;
+    mapping (uint256 => uint256) beneficiarySchemeCount;
 
     event VestingShemeCreation (TokenVesting.vestingScheme);
 
@@ -36,6 +39,7 @@ contract TokenVesting is Ownable {
 
     constructor (address _token) notZeroAddress (_token) {
         token = IERC20 (_token);
+        schemeCountLimit = 10;
     }
 
     receive () external payable {}
@@ -46,8 +50,28 @@ contract TokenVesting is Ownable {
     view 
     returns (vestingScheme memory) {
         require (_schemeId < schemeId, "TokenVesting: Scheme Id out of bounds");
-        require (schemes[_schemeId]._isValid == true, "TokenVesting: Vesting Scheme is either deleted or does not exist");
+        require (schemes[_schemeId].isValid == true, "TokenVesting: Vesting Scheme is either deleted or does not exist");
         return schemes[_schemeId];
+    }
+
+    function getUnvestedAmountById (uint256 _schemeId) external view returns (uint256) {
+        require (_schemeId < schemeId, "TokenVesting: Scheme Id out of bounds");
+        require (schemes[_schemeId].isValid == true, "TokenVesting: Vesting Scheme is either deleted or does not exist");
+        vestingScheme memory scheme = schemes[_schemeId];
+        uint256 unvestedAmount = scheme.amount.sub(scheme.tokensReleased);
+        return unvestedAmount;
+    }
+
+    function getSchemeCountLimit () external view returns (uint256) {
+        return schemeCountLimit;
+    }
+
+    function getSchemeCount () external view returns (uint256) {
+        return schemeCount;
+    } 
+
+    function setSchemeCountLimit (uint _schemeCountLimit) public onlyOwner{
+        schemeCountLimit = _schemeCountLimit;
     }
 
     function createVestingScheme (
@@ -57,9 +81,10 @@ contract TokenVesting is Ownable {
         uint256 _releaseSchedule,
         uint256 _amount
     ) external onlyOwner{
-        require (beneficiary != address(0), "TokenVesting: Cannot add zero address as beneficiary");
-        require (duration > 0, "TokenVesting: Vesting period must be greater than zero");
-        require (amount > 0, "TokenVesting: Vestable amount must be greater than 0");
+        require (_beneficiary != address(0), "TokenVesting: Cannot add zero address as beneficiary");
+        require (_duration > 0, "TokenVesting: Vesting period must be greater than zero");
+        require (_amount > 0, "TokenVesting: Vestable amount must be greater than 0");
+        require (beneficiarySchemeCount[_beneficiary] < 1, "TokenVesting: Only one vesting scheme per beneficiary is allowed at a time.");
         vestingScheme memory scheme = vestingScheme (
          _beneficiary,
          _startTime,
@@ -69,9 +94,11 @@ contract TokenVesting is Ownable {
          0,
          true);
 
+        beneficiarySchemeCount[scheme.beneficiary] = beneficiarySchemeCount[scheme.beneficiary].add(1);
+        schemeId = _calculateNewSchemeId(schemeCount, _beneficiary);
         schemes[schemeId] = scheme;
-         schemeId.add(1);
-         emit VestingShemeCreation (scheme);
+        schemeCount.add(1);
+        emit VestingShemeCreation (scheme);
     }
 
     function releaseTokens (
@@ -79,20 +106,23 @@ contract TokenVesting is Ownable {
         uint256 _amount
     ) 
     public {
-        require (schemes[schemeId]._isValid == true);//merge
+        require (schemes[schemeId].isValid == true);//merge
         require (_msgSender() == owner() || _msgSender() == schemes[schemeId].beneficiary, "TokenVesting: Caller must be owner or beneficiary");
 
         vestingScheme memory scheme = schemes[schemeId];
 
-        uint256 vestedAmount = _calculateReleaseableAmount (vestingScheme memory scheme);
-        require (_amount <= vestedAmount, "TokenVesting: Amount greater than vested amount as of now");
+        uint256 vestedAmount = _calculateReleaseableAmount (scheme);
+        require (_amount <= vestedAmount, "TokenVesting: Amount greater than current vested amount");
 
-        scheme.tokensReleased = scheme.tokensReleased.add(_amount)
-
-
+        scheme.tokensReleased = scheme.tokensReleased.add(_amount);
+        token.transfer(scheme.beneficiary, _amount);
     }
 
 
+
+    function _calculateNewSchemeId (address _beneficiary, uint256 index) internal view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(_beneficiary, index))) % 1000000000;
+    }
 
     function _now() internal virtual view returns (uint256) {
         return block.timestamp;
